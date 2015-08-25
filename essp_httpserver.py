26d0cb1c7b31878236a45217d5a344dbca149f4c
Efendy <max.begemot@gmail.com>
2015-08-25 17:19:42 +0300

    ok

--- essp_httpserver.py
+++ essp_httpserver.py
@@ -154,7 +154,8 @@ def essp_process(queue_request, queue_response, verbose, test):
             if cmd in cmds:
                 res['result'] = cmds[cmd]()()
             elif cmd == 'start':
-                res['result'] = bool(essp.sync() and essp.enable_higher_protocol() and essp.disable())
+                res['result'] = bool(essp.sync() and essp.enable_higher_protocol() and essp.disable() and
+                                     essp.set_inhibits(essp.easy_inhibit([1, 1, 1, 1, 1, 1, 1]), '0'))
             queue_response.put(res)
             continue
         poll = essp.poll()
